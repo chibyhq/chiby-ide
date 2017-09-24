d1754e08396489e75a959d45ec8b06ea0bc5acb0
Brice Copy <brice.copy@gmail.com>
2017-09-24 21:13:24 +0200

    Add hateoas and spring data rest support

--- modules/frontend/src/main/java/com/github/chiby/ide/frontend/FrontendApplication.java
+++ modules/frontend/src/main/java/com/github/chiby/ide/frontend/FrontendApplication.java
@@ -2,8 +2,12 @@ package com.github.chiby.ide.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.map.repository.config.EnableMapRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableMapRepositories("com.github.chiby.store.model.repositories")
+@EnableSpringDataWebSupport
 public class FrontendApplication {
 
 	public static void main(String[] args) {
