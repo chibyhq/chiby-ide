2b4e1eac896163f755bdf31400e99443e645616e
Brice Copy <brice.copy@gmail.com>
2017-10-02 14:54:54 +0200

    add ide welcome page

---
+++ modules/frontend/src/main/java/com/github/chiby/ide/frontend/SpringBootWelcomePageConfiguration.java
@@ -0,0 +1,27 @@
+package com.github.chiby.ide.frontend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Configure the welcome page 
+ * 
+ */
+@Configuration
+public class SpringBootWelcomePageConfiguration extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
+
+    /**
+     * redirect a user to the welcome page when he visits tha app without a
+     * destination url.
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+    	registry.addViewController("/ide").setViewName("redirect:/ide/");
+    	registry.addViewController("/ide/").setViewName("forward:/ide/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
