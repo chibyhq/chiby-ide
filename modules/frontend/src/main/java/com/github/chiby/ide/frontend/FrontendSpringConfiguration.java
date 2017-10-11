c38f2898c2f1cd5c86fc8b8ccb9b01054b79258d
Brice Copy <brice.copy@gmail.com>
2017-10-11 23:30:06 +0200

    Add typed bean for executors

--- modules/frontend/src/main/java/com/github/chiby/ide/frontend/FrontendSpringConfiguration.java
+++ modules/frontend/src/main/java/com/github/chiby/ide/frontend/FrontendSpringConfiguration.java
@@ -4,19 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.github.chiby.player.DockerExecutor;
-import com.github.chiby.player.IApplicationExecutor;
 import com.github.chiby.player.PygamezeroExecutor;
 
 @Configuration
 public class FrontendSpringConfiguration {
 
 	@Bean
-	IApplicationExecutor dockerExecutor(){
+	DockerExecutor dockerExecutor(){
 		return new DockerExecutor();
 	}
 	
 	@Bean
-	IApplicationExecutor pygamezeroExecutor(){
+	PygamezeroExecutor pygamezeroExecutor(){
 		return new PygamezeroExecutor();
 	}
 }
