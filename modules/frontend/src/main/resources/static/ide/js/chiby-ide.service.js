c992dc3efd4997f105e6368146e31a788c7bbfb4
Brice Copy <brice.copy@gmail.com>
2017-10-13 07:00:40 +0200

    Add delete functionality

--- modules/frontend/src/main/resources/static/ide/js/chiby-ide.service.js
+++ modules/frontend/src/main/resources/static/ide/js/chiby-ide.service.js
@@ -1,9 +1,18 @@
 function Chiby() {
+  
   this.statusTimer = null;
   this.statusUpdateCallbacks = [];
   this.applications = [];
+  this.runSessions = [];
   this.dataApi = hybind("/data");
-  this.dataApi.$bind("applications", this.applications);
+  var that = this;
+  this.applicationsCache = {};
+  this.dataApi.$bind("applications", this.applications).$load().then(function (){
+        that.applications.forEach(function(application) {
+                application.$share(that.applicationsCache);
+        });
+  });
+  this.dataApi.$bind("runSessions", this.runSessions);
   this.currentApp = null;
 }
 
@@ -19,19 +28,22 @@ Chiby.prototype.refreshHome = function(){
         // Empty the grid and populate it again
         $("#applications-grid").empty();
         $.each(that.applications, function(index,app){
-           var appItem = $('<div class="grid-item"></div>');
+           var appItem = $('<div id="'+app.uuid+'" class="grid-item"></div>');
            var innerPanel = $('<div class="panel panel-primary" style="height:280px"></div>');
            var innerPanelHeader = $('<div class="panel-heading"></div>');
            innerPanelHeader.text(app.title);
            innerPanel.append(innerPanelHeader);
            var avatar = 'question-sign';
            if(app.avatar != null) avatar = app.avatar;
-           var span = $('<div style="height:180px; padding:5px"><span style="font-size:8em; display:block; text-align:center" class="glyphicon glyphicon-'+avatar+'"></span><div style="padding:5px">'+app.description+'</div></div>');
+           var span = $('<div style="height:180px; padding:5px"><span style="font-size:8em; margin-top: 10px; color: #111111; display:block; text-align:center" class="glyphicon glyphicon-'+avatar+'"></span><div style="padding:5px">'+app.description+'</div></div>');
            innerPanel.append(span);
            var innerPanelFooter = $('<div class="panel-footer text-right" style="height:60px"></div>');
-           var editButton = $('<button type="button" class="btn btn-primary pull-right" onclick="chiby.openApplication($(this).data(\'app\'))">Edit</button>');
+           var editButton = $('<button type="button" class="btn btn-primary pull-right" style="margin:3px" onclick="chiby.openApplication($(this).data(\'app\'))">Open</button>');
            editButton.data('app', app);
+           var deleteButton = $('<button type="button" class="btn btn-danger pull-left" style="margin:3px" onclick="chiby.deleteApplication($(this).data(\'app\'))">Delete</button>');
+           deleteButton.data('app', app);
            innerPanelFooter.append(editButton);
+           innerPanelFooter.append(deleteButton);
            innerPanel.append(innerPanelFooter);
            appItem.append(innerPanel);
            $("#applications-grid").packery().append(appItem).packery('appended', appItem);
@@ -50,42 +62,43 @@ Chiby.prototype.openApplication = function(app){
      }
 }
 
-Chiby.prototype.createApplication = function(newApp){
-     $("#panel-home").collapse("hide");
-     $("#panel-program").collapse("show");
-     this.currentApp = app;
-     blocklyWorkspace.clear();
-     if(app.contents != null){
-       dom = Blockly.Xml.textToDom(app.contents);
-       Blockly.Xml.domToWorkspace(dom, blocklyWorkspace);
-     }
+Chiby.prototype.deleteApplication = function(app){
+      var uuid = app.uuid;
+      app.$delete().then(function(){
+              // this fails with a JQuery error in Sizzle
+              //var elem = $('#'+uuid);
+             // $("#applications-grid").packery('remove',elem);
+              chiby.refreshHome();
+      });
 }
 
+Chiby.prototype.createNewApplication = function(){
+    this.applications.$create(
+           {title: $('#new-app-title').val()
+           ,avatar: $('#new-app-avatar').val().toLowerCase().replace(/\s/g,"-")
+           ,description: $('#new-app-description').val()
+           ,type: 'PYGAMEZERO'}
+     ).then(function(newApplication){
+            chiby.refreshHome();
+     });
+}
+
+  
 Chiby.prototype.runApplication = function(){
-   if(this.currentApp != null){
-       this.currentApp.$save();
-       // Now create an association to a new runSession
+   var that = this;
+   if(that.currentApp != null){
+       that.currentApp.$save().then(function(savedApp){
+           // Now create an association to a new runSession
+           that.runSessions.$create({'applicationUUID':savedApp.uuid}).then(function(runSession){
+                  // runSession.$bind('application', savedApp).then(function(){
+                     // Fire the watcher thread to collect log outputs
+                     // and update the RUN button
+                     console.log("created run session !");
+                //   });
+           });
+       });
    }
 }
 
 
-Chiby.prototype.executeScript= function(scriptContents) {
-  $.ajaxSetup({ 
-    contentType: "application/json"
-  });
-  $.post( "/execute","{\"scriptText\":\"import time\\nprint 'hello world'\\ntime.sleep(10)\"}", function(reply) {
-    if(reply.success){
-      // Update the status immediately
-      chiby.updateStatus();
-    }else{
-      window.alert("Script execution failed : "+reply.message);
-    }
-  },"json");
-};
-
 chiby = new Chiby();
-
-/*
- * Blockly.Python.workspaceToCode(blocklyWorkspace)
- * Blockly.Xml.workspaceToDom(blocklyWorkspace)
- */
