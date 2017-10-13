8ac486b7fed7c5df14ab1b7d6666ae461535f173
Brice Copy <brice.copy@gmail.com>
2017-10-13 02:05:24 +0200

    implement various methods

--- modules/frontend/src/main/resources/static/ide/js/chiby-ide.service.js
+++ modules/frontend/src/main/resources/static/ide/js/chiby-ide.service.js
@@ -1,50 +1,72 @@
 function Chiby() {
   this.statusTimer = null;
   this.statusUpdateCallbacks = [];
+  this.applications = [];
+  this.dataApi = hybind("/data");
+  this.dataApi.$bind("applications", this.applications);
+  this.currentApp = null;
 }
 
 
-Chiby.prototype.start = function(){
-    console.log( "Started");
-    this.invokeGetStatusTimeout();
-};
+Chiby.prototype.showHome = function(){
+     $("#panel-program").collapse("hide");
+     $("#panel-home").collapse("show");
+}
 
-Chiby.prototype.addStatusUpdateCallback = function(listener){
-    this.statusUpdateCallbacks.push(listener);
-};
-
-Chiby.prototype.invokeGetStatusTimeout = function(){
-    chiby.updateStatus();
-    
-    chiby.statusTimer = setTimeout(function(){chiby.invokeGetStatusTimeout()}, 3000);
-};
-
-Chiby.prototype.updateStatus= function() {
-  $.getJSON( "/status", function(reply) {
-    console.log( "Chiby Status > Running? "+reply.running+" Exit Code : " +reply.exitCode );
-    
-    $.each(chiby.statusUpdateCallbacks, function( cb){
-            try{
-                this(reply);
-            }catch(err){
-                console.log("Could not update service status : "+err);
-            }
-    }, reply);
-
-  })
-  .fail(function() {
-    console.log( "error : Could not obtain chiby  status." );
-    $.each(chiby.statusUpdateCallbacks, function( cb ){
-            try{
-                this({running: -3, exitCode: -1});
-            }catch(err){
-                console.log("Could not update service status : "+err);
-            }
+Chiby.prototype.refreshHome = function(){
+    var that = this;
+    this.applications.$load().then(function(){
+        // Empty the grid and populate it again
+        $("#applications-grid").empty();
+        $.each(that.applications, function(index,app){
+           var appItem = $('<div class="grid-item"></div>');
+           var innerPanel = $('<div class="panel panel-primary" style="height:280px"></div>');
+           var innerPanelHeader = $('<div class="panel-heading"></div>');
+           innerPanelHeader.text(app.title);
+           innerPanel.append(innerPanelHeader);
+           var avatar = 'question-sign';
+           if(app.avatar != null) avatar = app.avatar;
+           var span = $('<div style="height:180px; padding:5px"><span style="font-size:8em; display:block; text-align:center" class="glyphicon glyphicon-'+avatar+'"></span><div style="padding:5px">'+app.description+'</div></div>');
+           innerPanel.append(span);
+           var innerPanelFooter = $('<div class="panel-footer text-right" style="height:60px"></div>');
+           var editButton = $('<button type="button" class="btn btn-primary pull-right" onclick="chiby.openApplication($(this).data(\'app\'))">Edit</button>');
+           editButton.data('app', app);
+           innerPanelFooter.append(editButton);
+           innerPanel.append(innerPanelFooter);
+           appItem.append(innerPanel);
+           $("#applications-grid").packery().append(appItem).packery('appended', appItem);
+        });
     });
-   
+}
 
-  });
-};
+Chiby.prototype.openApplication = function(app){
+     $("#panel-home").collapse("hide");
+     $("#panel-program").collapse("show");
+     this.currentApp = app;
+     blocklyWorkspace.clear();
+     if(app.contents != null){
+       dom = Blockly.Xml.textToDom(app.contents);
+       Blockly.Xml.domToWorkspace(dom, blocklyWorkspace);
+     }
+}
+
+Chiby.prototype.createApplication = function(newApp){
+     $("#panel-home").collapse("hide");
+     $("#panel-program").collapse("show");
+     this.currentApp = app;
+     blocklyWorkspace.clear();
+     if(app.contents != null){
+       dom = Blockly.Xml.textToDom(app.contents);
+       Blockly.Xml.domToWorkspace(dom, blocklyWorkspace);
+     }
+}
+
+Chiby.prototype.runApplication = function(){
+   if(this.currentApp != null){
+       this.currentApp.$save();
+       // Now create an association to a new runSession
+   }
+}
 
 
 Chiby.prototype.executeScript= function(scriptContents) {
@@ -62,3 +84,8 @@ Chiby.prototype.executeScript= function(scriptContents) {
 };
 
 chiby = new Chiby();
+
+/*
+ * Blockly.Python.workspaceToCode(blocklyWorkspace)
+ * Blockly.Xml.workspaceToDom(blocklyWorkspace)
+ */
